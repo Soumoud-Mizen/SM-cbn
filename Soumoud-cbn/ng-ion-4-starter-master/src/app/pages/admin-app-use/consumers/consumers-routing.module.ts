3d3acc1e629fa101882d91df8236f3230a15e40e
Soumoud-Mizen <soumoud.m@pixelstrade.com>
2020-08-10 11:58:57 +0100

    no changes

--- Soumoud-cbn/ng-ion-4-starter-master/src/app/pages/admin-app-use/consumers/consumers-routing.module.ts
+++ Soumoud-cbn/ng-ion-4-starter-master/src/app/pages/admin-app-use/consumers/consumers-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConsumersPage } from './consumers.page';
-
+// routes
 const routes: Routes = [
   {
     path: '',
